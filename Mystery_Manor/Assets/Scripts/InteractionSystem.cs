cca3ffb72e49f19ce262e7a1726488d1be2530be
Nadolny <nadolnyjoaoifpr@gmail.com>
2024-10-29 12:24:43 -0300

    SecondCommit
    Added the Interact system, need to add the TextMeshPro properly

--- Mystery_Manor/Assets/Scripts/InteractionSystem.cs
+++ Mystery_Manor/Assets/Scripts/InteractionSystem.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
+
 
 public class InteractionSystem : MonoBehaviour
 {
     // Start is called before the first frame update
-    public UnityEvent onInteract;
-    public Collider2D Interact_area;
+   public UnityEvent onInteract;
+   public bool InArea = false;
+   public TMP_Text InteractText;
+   
+    
     void Start()
     {
         
@@ -16,14 +21,28 @@ public class InteractionSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
+      if (InArea == true && Input.GetKeyDown(KeyCode.Space)){
+        InteractText = "Aperte Espaço para interagir";
+        
+        onInteract.Invoke();
+
+      }
     }
-    public void OnInteraction(){
+   public void OnInteraction(){
         onInteract.Invoke();
 
     }
-     void OnCollisionEnter(Collision collision)
-    {
-       Debug.Log("fonceonou");
+    void OnTriggerEnter2D (Collider2D collision) {
+    if(collision.gameObject.CompareTag("Player")){
+        Debug.Log("Primeira parte funciona");
+       
+            InArea = true;
+        }
+        
+        
+        
+       
+       
     }
-}
+    }
+
