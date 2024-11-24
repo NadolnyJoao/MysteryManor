6de56ae66bfd3f2f923cfa9067fbad06b13766ff
PedroVitor-oss <pedro.santos22@escola.pr.gov.br>
2024-11-24 17:48:11 -0300

    ajuste movimentação da camera  e add rosto pedra

--- Mystery_Manor/Assets/Scripts/Camera/CameraScript.cs
+++ Mystery_Manor/Assets/Scripts/Camera/CameraScript.cs
@@ -6,6 +6,7 @@ public class CameraScript : MonoBehaviour
 {
     public GameObject camera;
     public float speedMove = 2;
+    public  Vector3 targetPosition ;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,26 +17,26 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoveCamera();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Vector3 targetPosition = transform.position;
+         targetPosition = collision.gameObject.transform.position;
+        Debug.Log("name of sala "+ gameObject.name);
         targetPosition.z = -10.0f;
-        StartCoroutine(SmoothMoveCamera(targetPosition));
+        
     }
 
-    private IEnumerator SmoothMoveCamera(Vector3 targetPosition)
+    private void MoveCamera()
     {
         // Enquanto a posição da câmera não estiver próxima da posição desejada, continue a mover suavemente
-        while (Vector3.Distance(camera.transform.position, targetPosition) > 0.01f)
+        if (Vector3.Distance(camera.transform.position, targetPosition) > 0.1f)
         {
-            // Interpola entre a posição atual e a nova posição usando Lerp
+         
             camera.transform.position = Vector3.Lerp(camera.transform.position, targetPosition, speedMove * Time.deltaTime);
-            yield return null; // Espera o próximo frame
         }
 
-        // Garante que a câmera termine exatamente na posição alvo
-        camera.transform.position = targetPosition;
+        // // Garante que a câmera termine exatamente na posição alvo
+        // camera.transform.position = targetPosition;
     }
 }
